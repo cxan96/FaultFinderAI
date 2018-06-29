9b88df6c489a843ce9393078a09bcff46f4a59e2
cxan96 <c.p.xanten@gmail.com>
2018-06-29 10:49:08 +0200

    cycle through all faults

--- FaultFinderAI-TestingArea/src/main/java/testing/FaultExplorer.java
+++ FaultFinderAI-TestingArea/src/main/java/testing/FaultExplorer.java
@@ -4,16 +4,26 @@ import faulttypes.*;
 import org.nd4j.linalg.api.ndarray.*;
 import org.nd4j.linalg.util.NDArrayUtil;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class FaultExplorer {
     public static void main (String args []) {
 	
 	FaultFactory factory = new FaultFactory();
-	FaultData deadWire = factory.getFault(5);
 
-	
-	INDArray labelArray = NDArrayUtil.toNDArray(factory.getReducedLabel());
-	//                                          ^^^^^^^^^^^^^^^^^^^^^^^^^
-	
-	System.out.println(labelArray);
+	Scanner sc = new Scanner(System.in);
+
+	Random r = new Random();
+
+	for (int i = 0; i<50; i++) {
+	    int type = i%7;
+	    FaultData fault = factory.getFault(type);
+	    INDArray labelArray = NDArrayUtil.toNDArray(factory.getReducedLabel());
+	    System.out.println(labelArray);
+	    System.out.println(type);
+	    fault.plotData();
+	    sc.nextLine();
+	}
     }
 }
