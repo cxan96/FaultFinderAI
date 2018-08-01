e9995d51432b2366ca8a8f836e6b4bd28d6fb305
cxan96 <c.p.xanten@gmail.com>
2018-08-01 16:09:27 +0200

    new models and started validating

---
+++ FaultFinderAI-DL4J/src/main/java/client/ValidateRealData.java
@@ -0,0 +1,58 @@
+package client;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+import processHipo.DataProcess;
+import strategies.*;
+
+import faultTypes.FaultNames;
+
+public class ValidateRealData {
+
+    public static void main(String[] args) throws IOException{
+
+	String dataDir = "data/";
+		    
+	List<String> aList = new ArrayList<>();
+	aList.add(dataDir + "out_clas_003923.evio.80.hipo");
+	aList.add(dataDir + "out_clas_003923.evio.8.hipo");
+		
+	DataProcess dataProcess = new DataProcess(aList);
+	dataProcess.processFile();
+
+	FaultRecordScalerStrategy strategy = new MinMaxStrategy();
+
+	for (int sector = 1; sector < 2; sector++) {
+	    for (int superlayer = 1; superlayer < 7; superlayer++) {
+		double smallPinCertainty = getCertainty(FaultNames.PIN_SMALL, sector, dataProcess.getFeatureVector(sector, superlayer, strategy));
+		double bigPinCertainty = getCertainty(FaultNames.PIN_BIG, sector, dataProcess.getFeatureVector(sector, superlayer, strategy));
+		double deadWireCertainty = getCertainty(FaultNames.DEADWIRE, sector, dataProcess.getFeatureVector(sector, superlayer, strategy));
+		double hotWireCertainty = getCertainty(FaultNames.HOTWIRE, sector, dataProcess.getFeatureVector(sector, superlayer, strategy));
+
+		dataProcess.plotData(sector, superlayer);
+		
+		System.out.println("Small Pin: "+smallPinCertainty*100+"%");
+		System.out.println("Big Pin: "+bigPinCertainty*100+"%");
+		System.out.println("Dead Wire: "+deadWireCertainty*100+"%");
+		System.out.println("Hot Wire: "+hotWireCertainty*100+"%");
+
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+	    }
+	}
+
+    }
+
+    public static double getCertainty(FaultNames fault, int sector, INDArray data) throws IOException{
+	// get the model
+	FaultClassifier classifier = new FaultClassifier("models/binary_classifiers/sector"+sector+"/"+fault+".zip");
+	double[] predictions = classifier.output(data).toDoubleVector();
+	return predictions[0];
+    }
+}
--- FaultFinderAI-Faults/src/main/java/faultTypes/FaultNames.java
+++ FaultFinderAI-Faults/src/main/java/faultTypes/FaultNames.java
@@ -7,10 +7,10 @@ package faultTypes;
  * the label.
  */
 public enum FaultNames {
-	PIN_SMALL("Small Pin", 0), PIN_BIG("Big Pin", 1), CHANNEL_ONE("Channel 1", 2), CHANNEL_TWO("Channel 2",
+	PIN_SMALL("pin_small", 0), PIN_BIG("pin_big", 1), CHANNEL_ONE("Channel 1", 2), CHANNEL_TWO("Channel 2",
 			3), CHANNEL_THREE("Channel 3", 4), CONNECTOR_E("Connector E", 5), CONNECTOR_TREE("Connector Tree",
 					6), CONNECTOR_THREE("Connector Three", 7), FUSE_A("Fuse A", 8), FUSE_B("Fuse B", 9), FUSE_C(
-							"Fuse C", 10), DEADWIRE("Dead Wire", 11), HOTWIRE("Hot Wire", 12), NOFAULT("No Fault", 13);
+							"Fuse C", 10), DEADWIRE("deadwire", 11), HOTWIRE("hotwire", 12), NOFAULT("No Fault", 13);
 
 	private final String name;
 	private final int index;
